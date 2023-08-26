b2f62f12cae039f1b573c49d5e53342ec440e478
haroon-mirza <amirzaharoon@gmail.com>
2023-08-25 20:49:44 -0400

    added script that'll call multiple stocks

--- initial_api_test.py
+++ initial_api_test.py
@@ -1,27 +1,24 @@
 import requests
-import json
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 alpha_vantage_key = os.getenv('alpha_vantage_key')
 
-# Setting up the call
-url = "https://www.alphavantage.co/query"
+# Calling the API
+api_endpoint = 'https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))'
 params = {
-    "function": "TIME_SERIES_INTRADAY",
-    "symbol": "MSFT",
-    "interval": "5min",
-    "apikey": alpha_vantage_key
+    'apiKey': 'YOUR_API_KEY_HERE',
+    'format': 'json',
+    'pageSize': 5
 }
 
-# API call
-response = requests.get(url, params=params)
+# API Call
+response = requests.get(api_endpoint, params=params)
 
-# Parse the JSON response
-data = json.loads(response.text)
-
-# Print the latest stock data (for testing purposes)
-latest_data = data["Time Series (5min)"]
-latest_timestamp = list(latest_data.keys())[0]
-print("Latest data for MSFT at {}: {}".format(latest_timestamp, latest_data[latest_timestamp]))
+# Run
+if response.status_code == 200:
+    data = response.json()
+    print('API call successful. Data received:', data)
+else:
+    print('API call failed. Status code:', response.status_code)
